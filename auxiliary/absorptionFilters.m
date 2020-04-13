7ace28441170234385bce2e94006311a7b55b6f5
Sebastian Jiro Schlecht <45119263+SebastianJiroSchlecht@users.noreply.github.com>
2020-04-13 10:17:58 +0300

    Update absorptionFilters.m

--- auxiliary/absorptionFilters.m
+++ auxiliary/absorptionFilters.m
@@ -1,10 +1,10 @@
 function FIR = absorptionFilters(frequency, targetRT60, filterOrder, delays, fs)
-%absorptionFilters - Generate delay-proportional absorption filters 
+%absorptionFilters - Generate delay-proportional absorption filters
 %Generates an FIR filter with given filter order such that placed in a
 %recursion loop with a given delay, a specified reverberation time is
 %achieved. Multiple channels filters can be generated at once. The first
 %and last elements of frequency must equal 0 and fs/2, respectively.
-%   
+%
 %
 % Syntax:  FIR = absorptionFilters(unitFrequency, targetRT60, filterOrder, delays, fs)
 %
@@ -18,7 +18,7 @@ function FIR = absorptionFilters(frequency, targetRT60, filterOrder, delays, fs)
 % Outputs:
 %    FIR - FIR filter coefficients
 %
-% Example: 
+% Example:
 %    FIR = absorptionFilters([0; 5000; 24000], [1; 1; 0.5], 30, 100, 48000)
 %
 % Other m-files required: RT602slope
@@ -26,7 +26,7 @@ function FIR = absorptionFilters(frequency, targetRT60, filterOrder, delays, fs)
 % MAT-files required: none
 %
 % See also: absorption2T60
-% Author: Dr.-Ing. Sebastian Jiro Schlecht, 
+% Author: Dr.-Ing. Sebastian Jiro Schlecht,
 % Aalto University, Finland
 % email address: sebastian.schlecht@aalto.fi
 % Website: sebastianjiroschlecht.com
@@ -39,14 +39,21 @@ unitFrequency = hertz2unit(frequency,fs);
 %% compute filters per channel
 FIR = zeros(numberOfDelays, filterOrder+1);
 
-for ch = 1:numberOfDelays
-    rt60 =  targetRT60(:,ch);
-    delay = delays(ch);
-    
-    delay = delay + ceil(filterOrder/2);
-    db = delay*RT602slope(rt60, fs);
-    targetAmplitude = db2mag(db);
-    
-    %% Filter Approximation with window method
-    FIR(ch,:) = fir2(filterOrder,unitFrequency,targetAmplitude);
+if( filterOrder == 0)
+    rt60 =  targetRT60(1,:);
+    delay = delays;
+    db = delay.*RT602slope(rt60, fs);
+    FIR = db2mag(db);
+else
+    for ch = 1:numberOfDelays
+        rt60 =  targetRT60(:,ch);
+        delay = delays(ch);
+        
+        delay = delay + ceil(filterOrder/2);
+        db = delay*RT602slope(rt60, fs);
+        targetAmplitude = db2mag(db);
+        
+        %% Filter Approximation with window method
+        FIR(ch,:) = fir2(filterOrder,unitFrequency,targetAmplitude);
+    end
 end
