3715afde6c4cbb6be77576535680a0b4035fe326
Sebastian J. Schlecht <sebastianjiroschlecht@gmail.com>
2020-07-09 22:15:23 +0200

    add tests up to 14

--- Examples/example_dss2tf.m
+++ Examples/example_dss2tf.m
@@ -1,5 +1,8 @@
 % Example for dss2tf
 %
+% Convert delay state-space form of an FDN into the transfer function form
+% (dss2tf). Verify the translation by error in the impulse response.
+%
 % Sebastian J. Schlecht, Sunday, 13. January 2019
 clear; clc; close all;
 
@@ -7,7 +10,7 @@ rng(5)
 fs = 48000;
 impulseResponseLength = fs/4;
 
-%% Define FDN
+% Define FDN
 N = 4;
 numInput = 3;
 numOutput = 2;
@@ -17,12 +20,12 @@ D = randn(numOutput,numInput);
 delays = randi([50,100],[1,N]);
 A = randomOrthogonal(N);
 
-%% ss -> tf -> impz
+% ss -> tf -> impz
 [tfB,tfA] = dss2tf(delays,A,B,C,D);
 irTF = mtf2impz(tfB,tfA,impulseResponseLength);
 polesTF = roots(tfA);
 
-%% ss -> pr -> impz
+% ss -> pr -> impz
 [res, pol, direct, isConjugatePolePair] = dss2pr(delays,A,B,C,D);
 irPR = pr2impz(res, pol, direct, isConjugatePolePair, impulseResponseLength);
 irPR = permute(irPR,[2 3 1]);
@@ -30,10 +33,7 @@ irPR = permute(irPR,[2 3 1]);
 allPoles = restoreConjugatePairs(pol, isConjugatePolePair,'poles');
 allResidues = restoreConjugatePairs(res, isConjugatePolePair,'residues');
 
-%% compare
-maximumDeviation = max( abs(irPR - irTF), [], 3)
-
-%% plot
+% plot
 figure(1); hold on; grid on;
 plot(real(allPoles),imag(allPoles),'x');
 plot(real(polesTF),imag(polesTF),'o');
@@ -41,4 +41,11 @@ legend({'PR Poles','TF Poles'})
 xlabel('Real Axis')
 ylabel('Imaginary Axis')
 
- 
+%% Test: Impulse Response Accuracy
+assert(isAlmostZero(irPR - irTF, 'tol', 10^-10))
+
+%% Test: Pole Estimation Accuracy 
+poleError = sort(polesTF(imag(polesTF) >= 0),'ComparisonMethod','real') - sort(allPoles.','ComparisonMethod','real');
+assert(isAlmostZero(poleError, 'tol', 10^-10))
+
+
--- run_all_examples.m
+++ run_all_examples.m
@@ -7,7 +7,7 @@ fprintf('%s\n',fileNames{:})
 
 %% Run
 % fileNames = {fileNames{2}, fileNames{13}}
-results = runtests({fileNames{12}})
+results = runtests({fileNames{14}})
 table(results)
 
 % example_FDNdecorrelation
