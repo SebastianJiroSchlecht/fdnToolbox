1caaae4e3c17af8b8546f46384a7bb5fcbfc7361
Sebastian Jiro Schlecht <45119263+SebastianJiroSchlecht@users.noreply.github.com>
2021-04-02 17:41:46 +0300

    Update plotImpulseResponseMatrix.m

--- auxiliary/plotImpulseResponseMatrix.m
+++ auxiliary/plotImpulseResponseMatrix.m
@@ -3,8 +3,24 @@ function [plotAxes, plotHandles] = plotImpulseResponseMatrix( t, ir, varargin )
 %
 % Sebastian J. Schlecht, Sunday, 29 December 2019
 
+%% parse input
+p = inputParser;
+p.KeepUnmatched = true;
+addParameter(p,'xlabel',[]);
+addParameter(p,'ylabel',[]);
+addParameter(p,'xlim',[]);
+addParameter(p,'ylim',[]);
+parse(p,varargin{:});
+
+xLabel = p.Results.xlabel;
+yLabel = p.Results.ylabel;
+xLim = p.Results.xlim;
+yLim = p.Results.ylim;
+plotArg = [fieldnames(p.Unmatched), struct2cell(p.Unmatched)];
+
+%% plot
 if isempty(t)
-   t = 1:size(ir,3); 
+    t = 1:size(ir,3);
 end
 
 % change order to [FIR, out, in]
@@ -14,11 +30,37 @@ numberOfInputs = size(ir,3);
 
 for itOut = 1:numberOfOutputs
     for itIn = 1:numberOfInputs
-        plotAxes(itOut,itIn) = subplot(numberOfOutputs, numberOfInputs, sub2ind([numberOfInputs,numberOfOutputs], itIn, itOut)); 
+        plotAxes(itOut,itIn) = subplot(numberOfOutputs, numberOfInputs, sub2ind([numberOfInputs,numberOfOutputs], itIn, itOut));
         hold on; grid on;
-%         plotHandles(itOut,itIn) = reduce_plot(t, ir(:,itOut,itIn),varargin{:});
-        plotHandles(itOut,itIn) = stem(t, ir(:,itOut,itIn),varargin{:});
+        %         plotHandles(itOut,itIn) = reduce_plot(t, ir(:,itOut,itIn),varargin{:});
+        plotHandles(itOut,itIn) = stem(t, ir(:,itOut,itIn),plotArg{:});
+        
+        % plot options
+        if itIn == 1
+            if ~isempty(xLabel)
+                ylabel(yLabel);
+            end
+        else
+            set(gca, 'YTickLabel', [])
+        end
+        
+        if itOut == numberOfOutputs
+            if ~isempty(xLabel)
+                xlabel(xLabel);
+            end
+        else
+            set(gca, 'XTickLabel', [])
+        end
+        
+        if ~isempty(xLim)
+            xlim(xLim);
+        end
+        
+        if ~isempty(yLim)
+            ylim(yLim);
+        end
     end
+    
 end
 
 
