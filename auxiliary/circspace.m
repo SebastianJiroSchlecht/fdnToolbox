9f91dd76bca649cb701972334ce572c406a40617
Sebastian Jiro Schlecht <45119263+SebastianJiroSchlecht@users.noreply.github.com>
2021-04-22 22:37:49 +0300

    move files to auxiliary

--- auxiliary/circspace.m
+++ auxiliary/circspace.m
@@ -1,10 +1,7 @@
 function c = circspace(N)
-% function description
 % N points as rad degree without repetition of 0
 % 
 % (c) Sebastian Jiro Schlecht:  16. October 2018
-
-
 
 c = linspace(0,2*pi,N+1);
 c = c(1:end-1);
--- auxiliary/zp2rpk.m
+++ auxiliary/zp2rpk.m
@@ -1,22 +1,25 @@
 function [r,p,k] = zp2rpk(z,p,k)
 % convert zeros poles to pole residues (= partial fractions)
+% TODO: only works for single poles (no repeating poles)
+%
+% Sebastian J. Schlecht, Thursday, 22. April 2021
 
 assert(iscolumn(z))
 assert(iscolumn(p))
 
-% compute the denominator
+%% compute the denominator
 den = 1 - p./p.' ;
 den = den - diag(diag(den)) + eye(size(den));
 
 num = 1 - z./p.';
 
 numden = num./den;
-% r = k*prod(numden,1);
-% requires stable product, therefor it is sorted first
+
+
+%% requires stable product, therefor it is sorted first
 numdenA = sort(numden,1,'ascend','ComparisonMethod','abs');
-% numdenD = sort(numden,1,'descend','ComparisonMethod','abs');
-% numdenS = [numdenA(1:end/2,:); numdenD(1:end/2:end,:)];
-% interleaved sorting
+
+% interleaved sorting for best numerical conditioning
 numdenS = numdenA*0;
 numdenS(1:2:end,:) = numdenA(1:end/2,:);
 numdenS(2:2:end,:) = flip(numdenA(end/2+1:end,:),1);
@@ -25,10 +28,9 @@ r = k*prod(numdenS,1);
 
 r = r.';
 
-% use an arbitary position to test
+%% use an arbitary position to compute k
 zz = 1; 
 atzz = k*prod(1 - z./zz)./prod(1 - p./zz);
 atzz2 = sum(r ./ (1 - p./zz));
 
 k = atzz - atzz2;
-ok = 1;
