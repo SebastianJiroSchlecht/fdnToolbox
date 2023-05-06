1ff7f86dd86409a4952f7730ce08c985eb3f09dd
Sebastian Jiro Schlecht <sebastianjiroschlecht@gmail.com>
2023-05-06 22:49:10 +0300

    Add first order absorption filter

--- Examples/example_onePoleAbsorption.m
+++ Examples/example_onePoleAbsorption.m
@@ -1,4 +1,7 @@
 % (c) Sebastian Jiro Schlecht:  23. April 2018
+% 
+% Example of using a first order lowpass filter for frequency-dependent
+% reverberation time.
 clear; clc; close all;
 filename = mfilename;
 
@@ -10,19 +13,23 @@ impulseResponseLength = fs;
 N = 4;
 numInput = 1;
 numOutput = 1;
-inputGain = eye(N,numInput);
-outputGain = eye(numOutput,N);
+inputGain = ones(N,numInput);
+outputGain = ones(numOutput,N);
 direct = ones(numOutput,numInput);
-delays = randi([5, 30]*10,[1,N]);
+delays = randi([50, 300],[1,N]);
 feedbackMatrix = randomOrthogonal(N);
-
-
 
 % Generate absorption filters
 RT_DC = 2; % seconds
 RT_NY = 0.5; % seconds
+crossover_frequency = 4000; % Hz
 
-[absorption.b,absorption.a] = onePoleAbsorption(RT_DC, RT_NY, delays, fs);
+switch 'firstOrder'
+    case 'onePole'
+        [absorption.b,absorption.a] = onePoleAbsorption(RT_DC, RT_NY, delays, fs);
+    case 'firstOrder'
+        [absorption.b,absorption.a] = firstOrderAbsorption(RT_DC, RT_NY, crossover_frequency, delays, fs);
+end
 zAbsorption = zTF(absorption.b, absorption.a,'isDiagonal', true); 
 
 % compute with absorption
@@ -40,6 +47,7 @@ no_irResPol = pr2impz(no_res, no_pol, no_directTerm, no_isConjugatePolePair, imp
 
 % min max bounds
 [MinCurve,MaxCurve,w] = poleBoundaries(delays, absorption, feedbackMatrix);
+f = w / pi * fs/2;
 
 % plot
 figure(1); hold on; grid on;
@@ -55,15 +63,15 @@ plot(angle(no_pol),mag2db(abs(no_res)),'.','MarkerSize',1);
 legend({'Residues with attenuation','Residues without attenuation'},'Location','southeast')
 xlabel('Pole Angular Frequency [rad]')
 ylabel('Residue Magnitude [dB]')
-set(gca,'XLim',[0 pi],'YLim',[-120 -70]);
+% set(gca,'XLim',[0 pi],'YLim',[-120 -70]);
 
 figure(4); hold on; grid on;
-plotAxes = plot(angle(pol),slope2RT60(mag2db(abs(pol)), fs),'.','MarkerSize',1);
-plot(w,slope2RT60(mag2db(MinCurve),fs));
-plot(w,slope2RT60(mag2db(MaxCurve),fs)); 
-xlabel('Pole Angular Frequency [rad]')
+plotAxes = plot(angle(pol)/pi*fs/2,slope2RT60(mag2db(abs(pol)), fs),'.','MarkerSize',1);
+plot(f,slope2RT60(mag2db(MinCurve),fs));
+plot(f,slope2RT60(mag2db(MaxCurve),fs));
+xlabel('Pole Frequency [Hz]')
 ylabel('Pole RT60 [s]')
-set(gca,'YLim',[0 2.2],'XLim',[0 pi]);
+set(gca,'YLim',[0 2.2],'XLim',[100 fs/2],'XScale','log');
 legend({'Poles with attenuation','Minimum Bound','Maximum Bound'},'Location','northeast')
 
 %% Test: Impulse Response Accuracy
@@ -71,8 +79,8 @@ matMax = permute(max(abs(difference(1:end)),[],1),[2 3 1])
 assert( isAlmostZero(difference, 'tol', 10^-10 )  )
 
 %% Test: Poles between bounds 
-[isBounded, isSmaller] = isBoundingCurve(angle(pol),abs(pol),w,MaxCurve,'upper');
-assert(isBounded)
+[allBounded, isBounded] = isBoundingCurve(angle(pol),abs(pol),w,MaxCurve + 10^-6,'upper');
+assert(allBounded)
 
-[isBounded, isSmaller] = isBoundingCurve(angle(pol),abs(pol),w,MinCurve,'lower');
-assert(isBounded)
+[allBounded, isBounded] = isBoundingCurve(angle(pol),abs(pol),w,MinCurve - 10^-6,'lower');
+assert(allBounded)
---
+++ auxiliary/firstOrderAbsorption.m
@@ -0,0 +1,57 @@
+function [b,a] = firstOrderAbsorption(RT_DC, RT_NY, crossover_frequency, delays, fs)
+% firstOrderAbsorption - Design first-order shelving absorption filters according to specified T60
+%
+% see   Jot, J. M. Proportional parametric equalizers - Application to
+% digital reverberation and environmental audio Processing. in 1–8 (2015).
+%
+%
+% Syntax:  [b,a] = firstOrderAbsorption(RT_DC, RT_NY, delays, fs)
+%
+% Inputs:
+%    RT_DC - Reverberation time in [seconds] at DC frequency
+%    RT_NY - Reverberation time in [seconds] at Nyquist frequency
+%    crossover_frequency - TODO
+%    delays - Array of delay times in [samples]
+%    fs - Sampling frequency
+%
+% Outputs:
+%    b - Filter numerator of size [N,1,1]
+%    a - Filter denominators of size [N,1,2]
+%
+% Example: 
+%    [b,a] = firstOrderAbsorption(2, 1, [100, 130], 48000)
+%
+% Other m-files required: none
+% Subfunctions: none
+% MAT-files required: none
+%
+% See also: 
+% Author: Dr.-Ing. Sebastian Jiro Schlecht, 
+% Aalto University, Finland
+% email address: sebastian.schlecht@aalto.fi
+% Website: sebastianjiroschlecht.com
+% Saturday, 06 May 2023
+
+HDc = db2mag( delays * RT602slope( RT_DC, fs ) );
+HNyq = db2mag( delays * RT602slope( RT_NY, fs ) );
+
+omega = crossover_frequency / fs * 2*pi;
+
+[b,a] = designFirstOrderShelvingFilter(HDc, HNyq, omega);
+
+
+function [b,a] = designFirstOrderShelvingFilter(HDc, HNyq, omega)
+
+numFilters = numel(HDc);
+
+t = tan(omega);
+k = HDc ./ HNyq;
+
+r = t ./ sqrt(k);
+
+b(:,1,1) = (t .* sqrt(k) + 1) .* HNyq;
+b(:,1,2) = (t .* sqrt(k) - 1) .* HNyq;
+a(:,1,2) = t ./ sqrt(k) - 1;
+a(:,1,1) = t ./ sqrt(k) + 1;
+
+
