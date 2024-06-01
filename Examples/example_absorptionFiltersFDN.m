07053b6317489ecc7c5e5b4ec67689baa06d4460
Sebastian Jiro Schlecht <sebastianjiroschlecht@gmail.com>
2024-06-01 10:05:06 +0200

    Add more filters to the script

--- Examples/example_absorptionFiltersFDN.m
+++ Examples/example_absorptionFiltersFDN.m
@@ -23,10 +23,24 @@ delays = randi([500,2000],[1,N]);
 feedbackMatrix = randomOrthogonal(N);
 
 % absorption filters
-filterOrder = 64;
+% T60frequency = [0, 46, 63, 125, 250, 500, 1000, 2000, 4000, 8000, 11360, fs/2]'; % Hz
 T60frequency = [0, 63, 125, 250, 500, 1000, 2000, 4000, 8000, fs/2]'; % Hz
 targetT60 = [2 linspace(2,0.5,8) 0.5]'.^0.3;
-absorption = absorptionFilters(T60frequency, targetT60*ones(1,N), filterOrder, delays, fs);
+switch 'FIR'
+    case 'FIR'
+        filterOrder = 12;
+        absorption.b = absorptionFilters(T60frequency, targetT60*ones(1,N), filterOrder, delays, fs);
+        absorption.b = permute(absorption.b,[1 3 2]);
+        absorption.a = 1 + 0*absorption.b(:,:,1);
+    % case 'onePole'
+    %     [absorption.b,absorption.a] = onePoleAbsorption(targetT60(1), targetT60(end), delays, fs);
+    % case 'firstOrder'
+    %     crossover_frequency = 1000;
+    %     [absorption.b,absorption.a] = firstOrderAbsorption(targetT60(1), targetT60(end), crossover_frequency, delays, fs);
+    % case 'graphicEQ'
+    %     targetT60 = [2, 2, 2.2, 2.3, 2.1, 1.5, 1.1, 0.8, 0.7, 0.7];  % seconds
+    %     [absorption.b,absorption.a] = absorptionGEQ(targetT60, delays, fs);
+end
 
 % compute impulse response and poles/zeros
 % absorptionMatrix = polydiag( absorption );
@@ -36,7 +50,7 @@ absorption = absorptionFilters(T60frequency, targetT60*ones(1,N), filterOrder, d
 % irResPol = pr2impz(res, pol, directTerm, isConjugatePolePair, impulseResponseLength, 'lowMemory');
 
 % dsp based
-[FDN,F,B,C,D] = makeFDN_dsp(delays, feedbackMatrix, inputGain, outputGain, direct, permute(absorption,[1 3 2]));
+[FDN,F,B,C,D] = makeFDN_dsp(delays, feedbackMatrix, inputGain, outputGain, direct, absorption);
 
 [res, pol, directTerm, isConjugatePolePair,metaData] = dss2pr_dsp(F,B,C,D);
 irResPol = pr2impz(res, pol, directTerm, isConjugatePolePair, impulseResponseLength); % , 'lowMemory'
--- dspGraph/makeFDN_dsp.m
+++ dspGraph/makeFDN_dsp.m
@@ -8,7 +8,7 @@ C = dspMatrix(outputGain);
 D = dspMatrix(direct);
 
 Z = dspParallelDelay(delays);
-G = dspParallelFilters(absorption);
+G = dspParallelFilters(absorption.b,absorption.a);
 
 % connect dsp
 blockSize = 10000; % TODO: not used
